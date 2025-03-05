14d1ca896c4dcb020102c475f6822917d2807ac0
Harsh Singh <harsh.singh@innobitsystems.com>
2025-03-05 17:59:43 +0530

    Adding cookie parser and morgan

--- server/server.js
+++ server/server.js
@@ -3,20 +3,23 @@ import 'dotenv/config';
 import express from 'express';
 import helmet from 'helmet';
 import cors from 'cors';
+import morgan from 'morgan';
+import cookieParser from 'cookie-parser';
 import dbConnect from './services/dbConnect.js';
 import userRoutes from './user/userRoutes.js';
 import patientRoutes from './patient/patientRoutes.js';
 import errorHandler from './ErrorHandler.js';
 
 
-
+morgan('tiny');
 
 const app = express();
 
 app.use(helmet());
 app.use(cors());
-app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser())
 
 app.use('/login', userRoutes);
 app.use('/patient', patientRoutes);
